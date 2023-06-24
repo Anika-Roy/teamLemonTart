f49b61c095ba3c99ede551ed5fb7d0a30dc5ecda
Anika Roy <anika1234roy@gmail.com>
2023-06-24 14:36:55 +0530

    started working on output display

--- printing_weather.py
+++ printing_weather.py
@@ -1,7 +1,45 @@
-from utils import response
 from utils import ascii_art_dictionary
 
-selected= "2022-09-04 12:00:00"
+response={
+    "dt": 1661893200,
+    "main": {
+    "temp": 292.46,
+    "feels_like": 292.54,
+    "temp_min": 290.31,
+    "temp_max": 292.46,
+    "pressure": 1015,
+    "sea_level": 1015,
+    "grnd_level": 931,
+    "humidity": 80,
+    "temp_kf": 2.15
+    },
+    "weather": [
+    {
+        "id": 500,
+        "main": "Rain",
+        "description": "light rain",
+        "icon": "10n"
+    }
+    ],
+    "clouds": {
+    "all": 68
+    },
+    "wind": {
+    "speed": 2.66,
+    "deg": 210,
+    "gust": 3.58
+    },
+    "visibility": 10000,
+    "pop": 0.7,
+    "rain": {
+    "3h": 0.49
+    },
+    "sys": {
+    "pod": "n"
+    },
+    "dt_txt": "2022-08-30 21:00:00"
+}
+
 for item in response["list"]:
     if item["dt_txt"] == selected:
         print("weather: ",item["weather"][0]["main"])
