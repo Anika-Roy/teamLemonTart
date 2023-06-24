f2e702e1d0d2287fad7a92f377f258a7383e69c4
Ujjwal Shekhar <ujjwal.shekhar@research.iiit.ac.in>
2023-06-24 17:45:51 +0530

    Removed autocompletion feature

--- frontend/city_name_choose_cli.py
+++ frontend/city_name_choose_cli.py
@@ -1,18 +1,19 @@
 # Import the list of cities
 from utils.constants import CITY_LIST
-from prompt_toolkit import PromptSession
-from prompt_toolkit.completion import Completer, Completion
+import click
+# from prompt_toolkit import PromptSession
+# from prompt_toolkit.completion import Completer, Completion
 
 # Define your custom completer
-class MyCompleter(Completer):
-    def get_completions(self, document, complete_event):
-        # Implement your logic to provide completions
-        completions = CITY_LIST
-        word_before_cursor = document.get_word_before_cursor()
+# class MyCompleter(Completer):
+#     def get_completions(self, document, complete_event):
+#         # Implement your logic to provide completions
+#         completions = CITY_LIST
+#         word_before_cursor = document.get_word_before_cursor()
 
-        for completion in completions:
-            if completion.startswith(word_before_cursor):
-                yield Completion(completion, start_position=-len(word_before_cursor))
+#         for completion in completions:
+#             if completion.startswith(word_before_cursor):
+#                 yield Completion(completion, start_position=-len(word_before_cursor))
 
 
 
@@ -28,17 +29,15 @@ def return_city_name():
     """
 
     # Create a PromptSession with the custom completer
-    session = PromptSession(completer=MyCompleter())
+    # session = PromptSession(completer=MyCompleter())
 
     # Getting the city name from the user
-    city_name = session.prompt("Enter a City name: ")
+    # city_name = session.prompt("Enter a City name: ")
+
+    click.echo("Enter a City name: ")
+
+    city_name = click.prompt("Enter a City name: ")
+
+    return {"error": False, "city_name": city_name}
 
     # Error handling
-    if city_name in CITY_LIST:
-        # Returning the city name
-        return {"error": False, "city_name": city_name}
-        
-    else:
-        # Returning a json with error : True
-        return {"error": True, "message": "The city name is not in our database."}
-    
