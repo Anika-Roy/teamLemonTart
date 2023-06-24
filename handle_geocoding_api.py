12b4f6a59cfad822b0928d4c4efeab528d6f4083
Anika Roy <anika1234roy@gmail.com>
2023-06-24 13:37:26 +0530

    api call working

--- handle_geocoding_api.py
+++ handle_geocoding_api.py
@@ -1,6 +1,7 @@
 from prompt_toolkit import PromptSession
 from prompt_toolkit.completion import Completer, Completion
-
+import requests
+OPENWEATHERMAP_API_KEY="a35f0c612e92762a5f9f081e45cc3c3a"
 # # Define your custom completer
 # class MyCompleter(Completer):
 #     def get_completions(self, document, complete_event):
@@ -17,6 +18,11 @@ from prompt_toolkit.completion import Completer, Completion
 
 # # Use the session to prompt and handle user input
 # user_input = session.prompt("Enter a command: ")
+
+# api call to get weather report
+response=requests.get(f"https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={OPENWEATHERMAP_API_KEY}")
+print(response.json())
+
 
 
 
