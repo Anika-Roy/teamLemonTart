4e865a238d8fcd3e442f4bd93114db08f81c3b45
Anika Roy <anika1234roy@gmail.com>
2023-06-24 16:11:22 +0530

    display_forecast_cli copied

--- frontend/display_forecast_cli.py
+++ frontend/display_forecast_cli.py
@@ -1,7 +1,7 @@
 # Import the WEATHER_SYMBOL map from constant.py
-from utils.constants import WEATHER_SYMBOL
+from frontend_symbols import WEATHER_SYMBOL, ICON_CODE_MAPPING
 
-def get_weather_symbol(weather_main : int):
+def get_weather_symbol(weather_icon : int):
     """
     Returns ASCII art representation from a given
     main description from the forecast_json
@@ -19,12 +19,12 @@ def get_weather_symbol(weather_main : int):
     # Storing if the key was found
     key_found = False
 
-    # Iterating over the WEATHER_SYMBOL map
-    for key in WEATHER_SYMBOL:
-        # Checking if the weather_main is in the key
-        if weather_main in key:
+    # Iterating over the ICON_CODE_MAPPING map
+    for icon in ICON_CODE_MAPPING:
+        # Checking if the weather_icon is in the key
+        if weather_icon == icon:
             # Assigning the weather symbol
-            weather_symbol = WEATHER_SYMBOL[key]
+            weather_symbol = WEATHER_SYMBOL[ICON_CODE_MAPPING[icon]]
 
             # Assigning the key_found
             key_found = True
